b36abd0d857a1aacfff92d26c09c44d42a063751
Sarath <sarath.hacks@gmail.com>
2016-03-07 22:43:55 +0530

    File reader

---
+++ FileReader/filereader.js
@@ -0,0 +1,35 @@
+/*
+
+Filereader.js
+
+
+*/
+
+var fileInput = document.getElementById('input');
+
+ var output = document.getElementById('output');
+
+fileInput.addEventListener('change',openFile);
+
+function openFile(e){
+
+  var input = e.target;
+
+   var reader = new FileReader();
+
+   reader.onload = function(){
+     var dataUrl = reader.result;
+
+     output.src = dataUrl;
+
+   }
+
+   reader.readAsDataURL(input.files[0]);
+
+
+}
+output.addEventListener('error',function(event){
+
+  alert('Hello');
+
+});
