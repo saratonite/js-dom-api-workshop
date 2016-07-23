2c296b2501b84f7eeee45a3ed9b90d360c68775e
Sarath <sarath.hacks@gmail.com>
2016-07-23 20:39:44 +0530

    Added dom-styling

---
+++ dom-styling/script.js
@@ -0,0 +1,8 @@
+/* https://www.kirupa.com/html5/setting_css_styles_using_javascript.htm
+ */
+
+// Accessing Styles
+var superman = document.getElementById('superman');
+superman.style.backgroundColor="blue";
+superman.style.color = "red";
+superman.style.fontSize = "20px";
